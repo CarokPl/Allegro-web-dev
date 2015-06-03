715680d52c28efa1e2a7a8c4080acf14101b2b0a
allegroyouth <allegroyouth@gmail.com>
2015-06-03 18:40:09 +0200

    add handlebars.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
- $(function () {
-
-
-
-
-
+ $(document).ready(function () {
+     $.get("/api/post.json", function (data) {
+         var source = $("#message-template").html();
+         var template = Handlebars.compile(source);
+         $.each(data, function (index, value) {
+             var html = template(value);
+             $('#messages').append(html);
+         });
+     });
  })
