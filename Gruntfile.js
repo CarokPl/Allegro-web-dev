67a855ce7dccd0038b425ba8933acede89c85400
allegroyouth <allegroyouth@gmail.com>
2015-06-10 18:52:44 +0200

    add ulgify

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.initConfig({
         less: {
@@ -17,6 +18,14 @@ module.exports = function (grunt) {
                 tasks: ['less'],
             }
         },
+        uglify: {
+            my_target: {
+                files: {
+                    'js/dest/output.min.js': ['js/*.js']
+                }
+            }
+        }
+
 
     });
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
  $(document).ready(function () {
+     var source = $("#message-template").html();
+     var template = Handlebars.compile(source);
      $.get("/api/post.json", function (data) {
-         var source = $("#message-template").html();
-         var template = Handlebars.compile(source);
          $.each(data, function (index, value) {
              var html = template(value);
              $('#messages').append(html);
